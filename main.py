f6fa8bd1990de09fbb97bfaeb605a11ddebd61f3
cjromano18 <romanocristianjulian@gmail.com>
2024-08-04 16:35:05 -0300

    Subierndo el Desafio 1: Sistema de Gestion de Productos

---
+++ main.py
@@ -0,0 +1,105 @@
+import os
+import platform
+from productos import ProductoElectronico, ProductoAlimenticio, GestionProductos
+
+def limpiar_pantalla():
+    ''' Limpiar la pantalla según el sistema operativo'''
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear') # Para Linux/Unix/MacOs
+
+def mostrar_menu():
+    print("========== Menú de Gestión de Productos ==========")
+    print('1. Agregar Producto Electrónico')
+    print('2. Agregar Producto Alimenticio')
+    print('3. Buscar Producto por Código')
+    print('4. Actualizar Producto')
+    print('5. Eliminar Producto por Código')
+    print('6. Mostrar Todos los Productos')
+    print('7. Salir')
+    print('==================================================')
+
+def agregar_producto(gestion, tipo_producto):
+    try:
+        codigo = input('Ingrese código del producto: ')
+        nombre = input('Ingrese nombre del producto: ')
+        precio = float(input('Ingrese precio del producto: '))
+        cantidad = int(input('Ingrese cantidad en stock: '))
+
+        if tipo_producto == '1':
+            garantia = int(input('Ingrese garantía en años: '))
+            producto = ProductoElectronico(codigo, nombre, precio, cantidad, garantia)
+        elif tipo_producto == '2':
+            fecha_expiracion = input('Ingrese fecha de expiración (AAAA-MM-DD): ')
+            producto = ProductoAlimenticio(codigo, nombre, precio, cantidad, fecha_expiracion)
+        else:
+            print('Opción inválida')
+            return
+
+        gestion.crear_producto(producto)
+        input('Presione enter para continuar...')
+
+    except ValueError as e:
+        print(f'Error: {e}')
+    except Exception as e:
+        print(f'Error inesperado: {e}')
+
+def buscar_producto_por_codigo(gestion):
+    codigo = input('Ingrese el código del producto a buscar: ')
+    gestion.leer_producto(codigo)
+    input('Presione enter para continuar...')
+
+def actualizar_producto(gestion):
+    codigo = input('Ingrese el código del producto para actualizar: ')
+    precio = float(input('Ingrese el nuevo precio del producto: '))
+    cantidad = int(input('Ingrese la nueva cantidad en stock: '))
+    gestion.actualizar_producto(codigo, precio, cantidad)
+    input('Presione enter para continuar...')
+
+def eliminar_producto_por_codigo(gestion):
+    codigo = input('Ingrese el código del producto a eliminar: ')
+    gestion.eliminar_producto(codigo)
+    input('Presione enter para continuar...')
+
+def mostrar_todos_los_productos(gestion):
+    print('=============== Listado completo de los Productos ==============')
+    for producto in gestion.leer_datos().values():
+        if 'garantia' in producto:
+            print(f"Producto: {producto['nombre']} - con Garantía de: {producto['garantia']} años")
+        elif 'fecha_expiracion' in producto:
+            print(f"{producto['nombre']} - Fecha de Expiración: {producto['fecha_expiracion']}")
+        else:
+            print(f"{producto['nombre']} - Precio: {producto['precio']} - Cantidad: {producto['cantidad']}")
+    print('=================================================================')
+    input('Presione enter para continuar...')
+
+if __name__ == "__main__":
+    archivo_productos = 'productos_db.json'
+    gestion = GestionProductos(archivo_productos)
+
+    while True:
+        limpiar_pantalla()
+        mostrar_menu()
+        opcion = input('Seleccione una opción: ')
+
+        if opcion == '1' or opcion == '2':
+            agregar_producto(gestion, opcion)
+        
+        elif opcion == '3':
+            buscar_producto_por_codigo(gestion)
+
+        elif opcion == '4':
+            actualizar_producto(gestion)
+
+        elif opcion == '5':
+            eliminar_producto_por_codigo(gestion)
+
+        elif opcion == '6':
+            mostrar_todos_los_productos(gestion)
+
+        elif opcion == '7':
+            print('Saliendo del programa...')
+            break
+        else:
+            print('Opción no válida. Por favor, seleccione una opción válida (1-7)')
---
+++ productos.py
@@ -0,0 +1,196 @@
+'''
+Desafío 1: Sistema de Gestión de Productos
+
+Objetivo: Desarrollar un sistema para manejar productos en un inventario.
+
+Requisitos:
+
+    # Crear una clase base Producto con atributos como nombre, precio, cantidad en stock, etc.
+    # Definir al menos 2 clases derivadas para diferentes categorías de productos (por ejemplo, ProductoElectronico,
+    ProductoAlimenticio) con atributos y métodos específicos.
+    # Implementar operaciones CRUD para gestionar productos del inventario.
+    # Manejar errores con bloques try-except para validar entradas y gestionar excepciones.
+    # Persistir los datos en archivo JSON.
+'''
+
+
+import json
+
+class Producto:
+    def __init__(self, codigo, nombre, precio, cantidad):
+        self.__codigo = self.validar_codigo(codigo)
+        self.__nombre = nombre
+        self.__precio = self.validar_precio(precio)
+        self.__cantidad = self.validar_cantidad(cantidad)
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    
+    @property
+    def nombre(self):
+        return self.__nombre.capitalize()
+    
+    @property
+    def precio(self):
+        return self.__precio
+    
+    @property
+    def cantidad(self):
+        return self.__cantidad
+
+    @precio.setter
+    def precio(self, nuevo_precio):
+        self.__precio = self.validar_precio(nuevo_precio)
+
+    @cantidad.setter
+    def cantidad(self, nueva_cantidad):
+        self.__cantidad = self.validar_cantidad(nueva_cantidad)
+
+    def validar_codigo(self, codigo):
+        if not isinstance(codigo, str) or len(codigo) < 1:
+            raise ValueError("El código del producto debe ser una cadena no vacía.")
+        return codigo
+
+    def validar_precio(self, precio):
+        try:
+            precio_num = float(precio)
+            if precio_num <= 0:
+                raise ValueError("El precio debe ser un número positivo.")
+            return precio_num
+        except ValueError:
+            raise ValueError("El precio debe ser un número válido.")
+
+    def validar_cantidad(self, cantidad):
+        try:
+            cantidad_num = int(cantidad)
+            if cantidad_num < 0:
+                raise ValueError("La cantidad no puede ser negativa.")
+            return cantidad_num
+        except ValueError:
+            raise ValueError("La cantidad debe ser un número entero válido.")
+
+    def to_dict(self):
+        return {
+            "codigo": self.codigo,
+            "nombre": self.nombre,
+            "precio": self.precio,
+            "cantidad": self.cantidad
+        }
+
+    def __str__(self):
+        return f"{self.nombre} (Código: {self.codigo}) - Precio: {self.precio} - Cantidad: {self.cantidad}"
+
+class ProductoElectronico(Producto):
+    def __init__(self, codigo, nombre, precio, cantidad, garantia):
+        super().__init__(codigo, nombre, precio, cantidad)
+        self.__garantia = garantia
+
+    @property
+    def garantia(self):
+        return self.__garantia
+
+    def to_dict(self):
+        data = super().to_dict()
+        data["garantia"] = self.garantia
+        return data
+
+    def __str__(self):
+        return f"{super().__str__()} - Garantía: {self.garantia} años"
+
+class ProductoAlimenticio(Producto):
+    def __init__(self, codigo, nombre, precio, cantidad, fecha_expiracion):
+        super().__init__(codigo, nombre, precio, cantidad)
+        self.__fecha_expiracion = fecha_expiracion
+
+    @property
+    def fecha_expiracion(self):
+        return self.__fecha_expiracion
+
+    def to_dict(self):
+        data = super().to_dict()
+        data["fecha_expiracion"] = self.fecha_expiracion
+        return data
+
+    def __str__(self):
+        return f"{super().__str__()} - Fecha de Expiración: {self.fecha_expiracion}"
+
+class GestionProductos:
+    def __init__(self, archivo):
+        self.archivo = archivo
+
+    def leer_datos(self):
+        try:
+            with open(self.archivo, 'r') as file:
+                datos = json.load(file)
+        except FileNotFoundError:
+            return {}
+        except Exception as e:
+            raise Exception(f'Error al leer datos del archivo: {e}')
+        else:
+            return datos
+
+    def guardar_datos(self, datos):
+        try:
+            with open(self.archivo, 'w') as file:
+                json.dump(datos, file, indent=4)
+        except IOError as e:
+            print(f'Error al intentar guardar los datos en {self.archivo}: {e}')
+        except Exception as e:
+            print(f'Error inesperado: {e}')
+
+    def crear_producto(self, producto):
+        try:
+            datos = self.leer_datos()
+            codigo = producto.codigo
+            if not codigo in datos.keys():
+                datos[codigo] = producto.to_dict()
+                self.guardar_datos(datos)
+                print(f"Producto {producto.nombre} creado correctamente.")
+            else:
+                print(f"Ya existe producto con código '{codigo}'.")
+        except Exception as e:
+            print(f'Error inesperado al crear producto: {e}')
+
+    def leer_producto(self, codigo):
+        try:
+            datos = self.leer_datos()
+            if codigo in datos:
+                producto_data = datos[codigo]
+                if 'garantia' in producto_data:
+                    producto = ProductoElectronico(**producto_data)
+                elif 'fecha_expiracion' in producto_data:
+                    producto = ProductoAlimenticio(**producto_data)
+                else:
+                    producto = Producto(**producto_data)
+                print(f'Producto encontrado con código {codigo}')
+                print(producto)
+            else:
+                print(f'No se encontró producto con código {codigo}')
+        except Exception as e:
+            print(f'Error al leer producto: {e}')
+
+    def actualizar_producto(self, codigo, nuevo_precio, nueva_cantidad):
+        try:
+            datos = self.leer_datos()
+            if codigo in datos:
+                datos[codigo]['precio'] = nuevo_precio
+                datos[codigo]['cantidad'] = nueva_cantidad
+                self.guardar_datos(datos)
+                print(f'Producto con código {codigo} actualizado.')
+            else:
+                print(f'No se encontró producto con código {codigo}')
+        except Exception as e:
+            print(f'Error al actualizar el producto: {e}')
+
+    def eliminar_producto(self, codigo):
+        try:
+            datos = self.leer_datos()
+            if codigo in datos:
+                del datos[codigo]
+                self.guardar_datos(datos)
+                print(f'Producto con código {codigo} eliminado correctamente.')
+            else:
+                print(f'No se encontró producto con código {codigo}')
+        except Exception as e:
+            print(f'Error al eliminar el producto: {e}')
